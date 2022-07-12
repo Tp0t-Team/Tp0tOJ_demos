6b7181d63236a092c2d16952b78e3dfec18c82a0
LordCasser <591289332@qq.com>
2022-07-13 00:41:14 +0800

    add side-channel demo

--- simple-AES/simple_AES/entrypoint.sh
+++ simple-AES/simple_AES/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 echo $1 | tr -d '\n' | hexdump -ve '1/1 "%.2x\n"' > /home/HTTELab/data.txt
+chmod +x /home/HTTELab/server
+cd /home/HTTELab/ && ./server --port 8888 --capture traces.json.gz -static "traces.json.gz" -static "elmo.tgz" -static "firmware.tgz"
